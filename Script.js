dbee43477fec16288957a48053652e1a393941ce
Pranaavchavan <131269048+Pranaavchavan@users.noreply.github.com>
2025-03-19 18:17:01 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,30 @@
+/*let m=Math.max(10,15,20);
+console.log(m);//20
+
+
+let person = {
+     firstName: "Alice",
+     lastName:  "Johnson",
+     age: 25,
+     add: "Pune",
+     fullName : function(){
+        return this.firstName + " "+ this.lastName + " " + this.age+ " " + this.add;
+     }
+
+};
+console.log(person.fullName());//Alice Johnson */
+
+/*function Test(){ //resassignment is done
+   /* var x=10;
+     x=20;
+    console.log(x);
+}
+ Test();*/
+
+ function JS(){
+ var x=10;
+    var x=20;   
+    console.log(x);
+
+ }
+    JS();
---
+++ obj.js
@@ -0,0 +1,15 @@
+let car={
+    brand:"VOlkswagen",
+    model:"virtus",
+    year:2024,
+    color:"white"
+};
+console.log(car.color);
+console.log(car.year);
+console.log(car.brand);
+console.log(car.model);
+
+car.tyre="MRF";
+console.log(car.tyre);
+car.speed="200km/hr";
+console.log(car.speed);
